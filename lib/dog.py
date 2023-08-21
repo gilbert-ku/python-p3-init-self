d929d342c8cedba0b9018d514ef8d5de674786c9
gilbert-ku <gilbert45ku@gmail.com>
2023-08-21 22:11:50 +0300

    completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
-
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+
+fida = Dog("Fida", "Mutta")
+fida.breed = "Mutt"
+
+print(fida.name)
+print(fida.breed)
+
+
+# A parity bit, or check bit, is a bit added to a string of bits to ensure that the total number of 1-bits in the string is even or odd. Parity bits are used as the simplest form of error detecting code.
+
+# You have two parameters, one being the wanted parity (always 'even' or 'odd'), and the other being the binary representation of the number you want to check.
+
+# Your task is to return an integer (0 or 1), whose parity bit you need to add to the binary representation so that the parity of the resulting string is as expected.
+
+# Example:
+
+#   Parity: 'even'
+#   Bin: '0101010'
